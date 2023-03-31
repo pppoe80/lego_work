df02f9d1c4ae99040c71554387b90318818b2bba
pppoe80 <823441913@qq.com>
2023-03-31 21:30:21 +0800

    Finish

--- real_task_4/DataProcess.m
+++ real_task_4/DataProcess.m
@@ -1,2 +1,27 @@
-Files = ["BiggerK1NLData--","BiggerK2NLData--"...
-    "Data0-"];
+CKNLDF =  ["BiggerK1NLData--","BiggerK2NLData--"];
+LNDF =  ["Data0-","Data0+","Data-0","Data--","Data+0","Data+-","Data-+","Data++"];
+NLDF =   ["NLData0-","NLData0+","NLData-0","NLData--","NLData+0","NLData+-","NLData-+","NLData++"];
+for i = (1:2)
+    figure("Name","ChangedControl")
+    grid on;
+    data = readmatrix(CKNLDF(i));
+    plot(data(:,1),data(:,2));
+    print("DataChangedKLinearControl"+i,"-djpeg");
+end
+close all;
+for i = (1:8)
+    figure("Name","DataFromLinearControl")
+    grid on;
+    data = readmatrix(LNDF(i));
+    plot(data(:,1),data(:,2));
+    print("DataFromLinearControl"+i,"-djpeg");
+end
+close all;
+for i = (1:8)
+    figure("Name","DataFromNotLinearControl")
+    grid on;
+    data = readmatrix(NLDF(i));
+    plot(data(:,1),data(:,2));
+    print("DataFromNotLinearControl"+i,"-djpeg");
+end
+close all;

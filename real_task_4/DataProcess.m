08912122717f75ad21027c3bb07c1e94bc0e4f4b
pppoe80 <823441913@qq.com>
2023-03-31 23:55:06 +0800

    pic MATLAB completed

--- real_task_4/DataProcess.m
+++ real_task_4/DataProcess.m
@@ -1,6 +1,7 @@
 CKNLDF =  ["BiggerK1NLData--","BiggerK2NLData--"];
 LNDF =  ["Data0-","Data0+","Data-0","Data--","Data+0","Data+-","Data-+","Data++"];
 NLDF =   ["NLData0-","NLData0+","NLData-0","NLData--","NLData+0","NLData+-","NLData-+","NLData++"];
+pos = [0,-50;0,50;-50,0;-50,-50;50,0;50,-50;-50,50;50,50];
 for i = (1:2)
     figure("Name","ChangedControl")
     grid on;
@@ -25,3 +26,23 @@ for i = (1:8)
     print("DataFromNotLinearControl"+i,"-djpeg");
 end
 close all;
+for i = (1:8)
+    x_goal = pos(i,1);
+    y_goal = pos(i,2);
+    simout = sim("LySim.slx");
+    figure("Name","DataFromSim")
+    grid on;
+    plot(simout.yout{1}.Values.Data,simout.yout{2}.Values.Data);
+    print("DataFromSimulation"+i,"-djpeg");
+end
+close all;
+
+
+
+
+
+
+
+
+
+
